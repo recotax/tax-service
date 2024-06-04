5c70f6e604cc228b88d2778868166a442cda4de0
Dyony Rezende <dyony_rezende@live.com>
2024-06-04 18:13:14 -0300

    migration: (create companies table)

---
+++ src/migrations/1717535310283-CreateCompanyTable.ts
@@ -0,0 +1,35 @@
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
+
+export class CreateCompanyTable1717535310283 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: "companies",
+        columns: [
+          {
+            name: "id",
+            type: "int",
+            isPrimary: true,
+            isGenerated: true,
+            generationStrategy: "increment",
+          },
+          {
+            name: "company_name",
+            type: "varchar",
+            length: "255",
+          },
+          {
+            name: "CNPJ",
+            type: "varchar",
+            length: "18",
+          },
+        ],
+      }),
+      true
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable("companies");
+  }
+}
